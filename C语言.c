27d740a54dbdec87ae9912885d565f71e39ce540
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2022-12-07 18:36:00 +0800

    Add files via upload

--- C语言.c
+++ C语言.c
@@ -7372,3 +7372,37 @@
 
 
 //结构类型不可作为其成员的类型
+
+//练习
+int cmp(int x, int y)
+{
+    if (x >= y)
+        return x;
+    else
+        return y;
+}
+int main()
+{
+    int a = 0;
+    int b = 0;
+    scanf("%d%d", &a, &b);
+    int ret = cmp(a, b);
+    printf("%d\n", ret);
+    return 0;
+}
+
+//练习2
+int main()
+{
+    float f = 0.0;
+    int i = 0;
+    float sum = 0.0;
+    while (i < 5)
+    {
+        scanf("%f", &f);
+        sum += f;
+        i++;
+    }
+    printf("%.1f", sum / 5);
+    return 0;
+}
